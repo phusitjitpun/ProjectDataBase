6e3d163de3b81825e60babf3eb3247c58bb1740d
phusitsiriyakorn <phusitsiriyakorn@gmail.com>
2021-04-12 20:44:10 +0700

    12

---
+++ TableDB/CREATE/CREATE_TABLEGOLDTH.py
@@ -0,0 +1,28 @@
+import psycopg2
+from psycopg2 import Error 
+
+try:
+    #connection = psycopg2.connect(user="postgres",password="1234",host="127.0.0.1",port="5432",database="pythonlogin")
+    connection = psycopg2.connect(user="webadmin",password="BFCqhr46914", host="node4943-env-2254395.th.app.ruk-com.cloud", port="11043", database="pythonlogin")
+
+    cursor = connection.cursor()
+
+    create_table_guery = '''CREATE TABLE GoldTH
+        (D_M_Y VARCHAR(50) ,
+        hard_buy     VARCHAR(50) ,
+        hard_sell     VARCHAR(50) ,
+        pic_buy      VARCHAR(50) ,
+        pic_sell      VARCHAR(50)); '''
+
+    cursor.execute(create_table_guery)
+    connection.commit()
+    print("Table created successfully in PostgreSOL ")
+
+except (Exception, psycopg2.DatabaseError) as error: 
+    print("Error while creating PostgreSQL table", error) 
+finally:
+    #closing database connection.
+    if(connection):
+        cursor.close()
+        connection.close()
+        print("PostgreSOL connection is closed")
